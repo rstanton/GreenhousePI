b1d2678375a5a0cecd636466502bdecb18e4cf30
Ross Stanton <ross_stanton@hotmail.com>
2018-11-10 10:24:11 +0000

    test of shift

--- src/main/java/com/stanton/i2c/sensor/BMP180.java
+++ src/main/java/com/stanton/i2c/sensor/BMP180.java
@@ -22,9 +22,9 @@ public class BMP180 {
 			int AC1 = data[0] <<8 + data[1];
 			int AC2 = data[2] * 256 + data[3];
 			int AC3 = data[4] * 256 + data[5];
-			int AC4 = ((data[6] & 0xFF) * 256) + (data[7] & 0xFF);
-			int AC5 = ((data[8] & 0xFF) * 256) + (data[9] & 0xFF);
-			int AC6 = ((data[10] & 0xFF) * 256) + (data[11] & 0xFF);
+			int AC4 = ((data[6] & 0xFF) <<8) + (data[7] & 0xFF);
+			int AC5 = ((data[8] & 0xFF) <<8) + (data[9] & 0xFF);
+			int AC6 = ((data[10] & 0xFF) <<8) + (data[11] & 0xFF);
 			int B1 = data[12] * 256 + data[13];
 			int B2 = data[14] * 256 + data[15];
 			int MB = data[16] * 256 + data[17];
